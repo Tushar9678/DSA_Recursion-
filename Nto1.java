e646bd14a625e5b9ebd207767f55a210d51f818c
Tushar Pawar <126349113+Tushar9678@users.noreply.github.com>
2024-02-09 17:56:35 +0530

    Nto1.java

--- Nto1.java
+++ Nto1.java
@@ -2,7 +2,10 @@
 
 public class Nto1{
     public static void main(String[] args){
-       solution(5);
+        System.out.println("printing numbers from 5 to 1");
+        solution(5);
+        System.err.println("printing numbers from 1 to 5");
+        fun_rev(5);
 
         
     }
@@ -13,5 +16,16 @@ public class Nto1{
         }
         System.out.println(n);
         solution(n-1);
+        
+    }
+
+    // print numbers 1 to 5
+    static void fun_rev(int n){
+        if(n <= 0) {
+            return;
+        }
+        fun_rev(n - 1);
+        System.out.println(n);
     }
 }
+
